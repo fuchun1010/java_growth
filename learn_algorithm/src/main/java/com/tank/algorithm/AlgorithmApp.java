e73f435d2f3c57c4f75965610ecc70ed6517d2bb
fuchun198435163.com <tank198435@163.com>
2020-11-01 06:57:54 +0800

    [fuchun] #tech new argorithem module

---
+++ learn_algorithm/src/main/java/com/tank/algorithm/AlgorithmApp.java
@@ -0,0 +1,10 @@
+package com.tank.algorithm;
+
+/**
+ * @author tank198435163.com
+ */
+public class AlgorithmApp {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}

0fc745898f9fd16bb660cb4b6be1b530568a8744
fuchun198435163.com <tank198435@163.com>
2020-11-08 19:40:53 +0800

    [fuchun] #tech single sort

--- learn_algorithm/src/test/java/com/tank/algorithm/sort/MergeSortingTest.java
+++ learn_algorithm/src/test/java/com/tank/algorithm/sort/MergeSortingTest.java
@@ -1,9 +1,13 @@
 package com.tank.algorithm.sort;
 
+import lombok.NonNull;
 import lombok.val;
+import lombok.var;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class MergeSortingTest {
 
@@ -13,7 +17,7 @@ public class MergeSortingTest {
     val b = 2;
     Assert.assertEquals(a / b, 4);
   }
-  
+
 
   @Test
   public void testMergeSort() {
@@ -28,6 +32,50 @@ public class MergeSortingTest {
     this.mergeSorting = new MergeSorting();
     this.mergeSorting2 = new MergeSorting();
   }
+
+
+  @Test
+  public void testMerge() {
+    int[] expected = {1, 2, 3, 4, 5};
+    int[] result = this.merge(new int[]{1, 3}, new int[]{2, 4, 5});
+    Assert.assertEquals(expected, result);
+  }
+
+
+  private int[] merge(@NonNull int[] left, @NonNull int[] right) {
+    int[] result = new int[left.length + right.length];
+
+    var index = 0;
+    while (left.length > 0 && right.length > 0) {
+      if (left[0] < right[0]) {
+        result[index] = left[0];
+        index++;
+        left = Arrays.copyOfRange(left, 1, left.length);
+      } else {
+        result[index] = right[0];
+        index++;
+        right = Arrays.copyOfRange(right, 1, right.length);
+      }
+
+    }
+
+    while (left.length > 0) {
+      result[index] = left[0];
+      index++;
+      left = Arrays.copyOfRange(left, 1, left.length);
+    }
+
+    while (right.length > 0) {
+      result[index] = right[0];
+      index++;
+      right = Arrays.copyOfRange(right, 1, right.length);
+    }
+
+
+    return result;
+
+  }
+
 
   private MergeSorting mergeSorting;
 

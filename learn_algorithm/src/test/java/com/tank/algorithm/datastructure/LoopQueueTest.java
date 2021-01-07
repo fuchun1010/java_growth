7d5b26e6438a3b7687baa695314ef3e9d45ffc8e
fuchun198435163.com <tank198435@163.com>
2021-01-08 07:39:42 +0800

    [fuchun] #tech spike

--- learn_algorithm/src/test/java/com/tank/algorithm/datastructure/LoopQueueTest.java
+++ learn_algorithm/src/test/java/com/tank/algorithm/datastructure/LoopQueueTest.java
@@ -1,5 +1,7 @@
 package com.tank.algorithm.datastructure;
 
+import lombok.val;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,12 +12,11 @@ public class LoopQueueTest {
   @Test
   public void enQueue() {
     IntStream.rangeClosed(1, 7).boxed().forEach(this.queue::enQueue);
-
+    val result = this.queue.deQueue();
+    Assert.assertNotNull(result);
+    Assert.assertEquals(1, (int) result);
   }
-
-  @Test
-  public void deQueue() {
-  }
+  
 
   @Before
   public void initialize() {
